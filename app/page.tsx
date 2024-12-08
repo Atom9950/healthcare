50fb5f6260eb3741b846f932fd26ad80e5a67910
Sayandeep <sdroy001@gmail.com>
2024-12-08 21:59:41 +0530

    Updated files and new packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import PatientForm from "@/components/forms/PatientForm"
 import Link from "next/link"
+import {PasskeyModal} from "@/components/PasskeyModal"
+import { useSearchParams } from 'next/navigation'
+import { useState, useEffect } from 'react'
 
-export default function Home() {
-  return(
+const Home = () => {
+  const searchParams = useSearchParams();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    setIsAdmin(searchParams.get('admin') === 'true');
+  }, [searchParams]);
+
+  return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image src={"/assets/icons/logo-full.svg"} height={1000} width={1000} className="mb-10 h-12 w-fit" alt={"patient"}/>
+          <Image 
+            src={"/assets/icons/logo-full.svg"} 
+            height={1000} 
+            width={1000} 
+            className="mb-10 h-12 w-fit" 
+            alt={"patient"}
+          />
 
           <PatientForm/>
-
         </div>
 
         <div className="text-14-regular mt-20 flex justify-between">
@@ -19,13 +38,18 @@ export default function Home() {
            © 2024 CarePulse
           </p>
           <Link href="/?admin=true" className="text-green-500">Admin</Link>
-        
         </div> 
-
       </section>
 
-      <Image src={"/assets/images/onboarding-img.png"} height={1000} width={1000} className="side-img max-w-[50%]" alt={"patient"}/>
-
+      <Image 
+        src={"/assets/images/onboarding-img.png"} 
+        height={1000} 
+        width={1000} 
+        className="side-img max-w-[50%]" 
+        alt={"patient"}
+      />
     </div>
   )
 }
+
+export default Home
